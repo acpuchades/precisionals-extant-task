8e39f826e380f1257bb4b174ed2caf0040961508
Alejandro Caravaca Puchades <59510094+acpuchades@users.noreply.github.com>
2024-06-02 19:07:12 +0200

    Use mixed-effects cox for SOD1 variant analysis

--- src/q3/sod1.r
+++ src/q3/sod1.r
@@ -1,5 +1,6 @@
 library(coxme)
 library(magrittr)
+library(stringr)
 library(survival)
 library(writexl)
 
@@ -8,85 +9,71 @@ source("src/ext/common.r")
 ext_source("src/q3/helpers.r")
 ext_source("src/q3/timetoevent.r")
 
-q3_sod1_dataset <- q3_data_w %>%
-  filter(sod1_status == "Positive" | !is.na(sod1_variant_p) | altered_genes == 0) %>%
-  mutate(sod1_variant_p = case_when(
+q3_analyze_t2e_by_sod1_variant <- function(data, prefix) {
+  sink(str_c(prefix, "sod1-analysis.txt"))
+
+  cat("# SUMMARY TABLE\n\n")
+  sod1_variant_freq <- data.frame(with(data, table(sod1_variant_p)))
+  sod1_variant_freq <- subset(sod1_variant_freq, sod1_variant_p != "None")
+  print(sod1_variant_freq)
+  cat("\n\n")
+
+  sod1_time_to_onset <- with(
+    data %>%
+      q3_select_event("birth", "onset", censor_after_epochs = 100 * 12) %>%
+      mutate(across(c(site, site_of_onset), fct_drop)),
+    coxme(Surv(time, status) ~ sex + site_of_onset + sod1_variant_p + (1 | site))
+  )
+
+  cat("# TIME TO ONSET\n\n")
+  print(tidy(sod1_time_to_onset))
+  cat("\n\n")
+
+  sod1_time_to_death <- with(
+    data %>%
+      q3_select_event("onset", "death", censor_after_epochs = 10 * 12) %>%
+      mutate(across(c(site, site_of_onset), fct_drop)),
+    coxme(Surv(time, status) ~ sex + site_of_onset + age_at_onset + sod1_variant_p + (1 | site))
+  )
+
+  cat("# TIME TO DEATH FROM ONSET\n\n")
+  print(tidy(sod1_time_to_death))
+  sink()
+}
+
+# Note: for some reason, we don't have data on SOD1 for patients from Trinity, so we can't
+#       simply include only those marked as negative without excluding the entire Ireland
+#       cohort (we have no comparator for the only variant carrier reported there).
+
+q3_sod1_dataset_global <- q3_data_w %>%
+  filter(
+    !is.na(sod1_status) | !is.na(sod1_variant_p),
+    site != "Cohort 7", site_of_onset != "Generalized",
+  ) %>%
+  mutate(sod1_variant_p = fct_relevel(case_when(
     !is.na(sod1_variant_p) ~ sod1_variant_p,
-    # sod1_status == "Negative" ~ "none",
-    altered_genes == 0 ~ "none",
-  ))
+    sod1_status == "Negative" ~ "None",
+  ), "None"))
 
 q3_sod1_dataset_recent <- q3_data_recent_w %>%
-  filter(sod1_status == "Positive" | !is.na(sod1_variant_p) | altered_genes == 0) %>%
-  mutate(sod1_variant_p = case_when(
+  filter(
+    !is.na(sod1_status) | !is.na(sod1_variant_p),
+    site != "Cohort 7", site_of_onset != "Generalized",
+  ) %>%
+  mutate(sod1_variant_p = fct_relevel(case_when(
     !is.na(sod1_variant_p) ~ sod1_variant_p,
-    # sod1_status == "Negative" ~ "none",
-    altered_genes == 0 ~ "none",
-  ))
+    sod1_status == "Negative" ~ "None",
+  ), "None"))
 
-q3_sod1_output_dir <- file.path(q3_output_root_dir, "sod1")
-dir.create(q3_sod1_output_dir, recursive = TRUE, showWarnings = FALSE)
+ext_interactive({
+  q3_sod1_output_dir <- file.path(q3_output_root_dir, "sod1")
+  dir.create(q3_sod1_output_dir, recursive = TRUE, showWarnings = FALSE)
 
-sink(file.path(q3_sod1_output_dir, "entire-cohort.txt"))
+  q3_show_progress("Analyzing SOD1 variant data on the global cohort", {
+    q3_analyze_t2e_by_sod1_variant(q3_sod1_dataset_global, file.path(q3_sod1_output_dir, "global-"))
+  })
 
-cat("# SUMMARY TABLE\n\n")
-sod1_variant_freq <- data.frame(with(q3_sod1_dataset, table(sod1_variant_p)))
-sod1_variant_freq <- subset(sod1_variant_freq, sod1_variant_p != "none")
-print(sod1_variant_freq)
-cat("\n\n")
-
-q3_sod1_onset_global <- with(
-  q3_select_event(q3_sod1_dataset, "birth", "onset", censor_after_epochs = 100 * 12),
-  coxme(Surv(time, status) ~ sex + site_of_onset + sod1_variant_p + (1 | site))
-)
-
-cat("# TIME TO ONSET (Entire cohort)\n\n")
-q3_sod1_onset_global %>%
-  summary() %>%
-  print()
-cat("\n\n")
-
-q3_sod1_survival_global <- with(
-  q3_select_event(q3_sod1_dataset, "onset", "death", censor_after_epochs = 10 * 12),
-  coxme(Surv(time, status) ~ sex + site_of_onset + age_at_onset + sod1_variant_p + (1 | site))
-)
-
-cat("# TIME TO DEATH FROM ONSET (Entire cohort)\n\n")
-q3_sod1_survival_global %>%
-  summary() %>%
-  print()
-sink()
-
-sink(file.path(q3_sod1_output_dir, "recent-cohort.txt"))
-
-cat("# SUMMARY TABLE\n\n")
-sod1_variant_freq_recent <- data.frame(with(q3_sod1_dataset_recent, table(sod1_variant_p)))
-sod1_variant_freq_recent <- subset(sod1_variant_freq, sod1_variant_p != "none")
-print(sod1_variant_freq_recent)
-cat("\n\n")
-
-q3_sod1_onset_recent <- with(
-  q3_select_event(q3_sod1_dataset_recent, "birth", "onset", censor_after_epochs = 100 * 12),
-  coxme(Surv(time, status) ~ sex + site_of_onset + sod1_variant_p + (1 | site))
-)
-
-cat("# TIME TO ONSET (Recent cohort)\n\n")
-q3_sod1_onset_recent %>%
-  summary() %>%
-  print()
-cat("\n\n")
-
-png("output/q3/sod1/coxzph-onset-recent.png")
-plot(cox.zph(q3_sod1_onset_recent), col = "red")
-dev.off()
-
-q3_sod1_survival_recent <- with(
-  q3_select_event(q3_sod1_dataset_recent, "onset", "death", censor_after_epochs = 10 * 12),
-  coxme(Surv(time, status) ~ sex + site_of_onset + age_at_onset + sod1_variant_p + (1 | site))
-)
-
-cat("# TIME TO DEATH FROM ONSET (Recent cohort)\n\n")
-q3_sod1_survival_recent %>%
-  summary() %>%
-  print()
-sink()
+  q3_show_progress("Analyzing SOD1 variant data on the recent cohort", {
+    q3_analyze_t2e_by_sod1_variant(q3_sod1_dataset_recent, file.path(q3_sod1_output_dir, "recent-"))
+  })
+})
